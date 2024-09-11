e56b092ef42a93a72dfcee2e5ee802b4c35123ff
Ritvik-UX <ritvikux@gmail.com>
2024-09-11 21:30:59 +0530

    refactor: based on understanding

--- src/index.rs
+++ src/index.rs
@@ -1,10 +1,39 @@
+// Persistent Index
+
+// file path index stored under: file_index_<thread_id> dir.
+// tfidf index stored under:     tfidf_index_<thread_id> dir.
+
+// For each flush of any index type,
+// one segment data file and one of more segment metadata files are created.
+
+// Segment metadata files contain hashtable entries for each index type.
+// Example: TFIDF Index segment metadata will contain list of IFDIndexEntry elements (array) in binary format.
+//
+// Segment data files contain auxiliary data structure elements.
+// In FilePathIndex, the auxiliary structure is actual full file path and corresponding element
+// from metadata file stores an offset to position where full file path can be found in the
+// data file.
+// TFIDFIndex, segment data file contains a list of lists of TFIndexEntry elements of size IndexDepth
+
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-use crate::util::Counter;
+// use crate::util::Counter;
 
+// During indexing, the Indexer will perform
+// lookups in the hash table and create new IDFIndexEntries or
+// TFIndexEntries if they don’t exist and update the frequency
+// information for each term-file pair.
 #[derive(Debug)]
 pub struct Index {
     pub inverted_index: InvertedIndex,
+
+    // TODO: FilePathIndex?
+    // The FilePathIndex component is a memoryintensive component and is responsible for computing a file index from
+    // the full file path and storing the index and the full file path into the
+    // inverted index in order to be retrieved during search operations. The
+    // file content, under the form of a list of extracted tokens, is then indexed by the TFIDFIndex, which is also a memory-intensive component
+    // and that indexes the tokens and keeps track of the term frequencies
+    // and inverse document frequencies necessary for computing the relevance score.
     pub file_index: FileIndex,
 }
 
@@ -15,17 +44,22 @@ pub struct FileIndex {}
 pub struct InvertedIndex {
     // Key = Token Index
     // Value = Index Entry
-    inner: HashMap<usize, Rc<RefCell<IdfIndexEntry>>>,
+    inner: HashMap<String, Rc<RefCell<IdfIndexEntry>>>,
 }
 
+// SCANNS
+// IDFIndexEntry keeps track of the token associated with the entry,
+// the number of files that contain the term,
 #[derive(Debug, Default)]
 pub struct IdfIndexEntry {
-    /// Token
-    token: String,
+    // Token
+    // - maybe the key
+    // token: String,
 
-    /// Number of files that contain the term.
-    file_count: Counter<usize>,
-
+    // Number of files that contain the term.
+    // - maybe the self.entries.len()
+    //
+    // file_count: Counter<usize>,
     entries: Vec<TfIndexEntry>,
 
     // Limit after which the data will be flushed into the disk.
@@ -40,10 +74,10 @@ impl IdfIndexEntry {
         }
     }
 
-    pub fn with_file_count(mut self, count: Counter<usize>) -> Self {
-        self.file_count = count;
-        self
-    }
+    // pub fn with_file_count(mut self, count: Counter<usize>) -> Self {
+    //     self.file_count = count;
+    //     self
+    // }
 
     pub fn with_threshold(mut self, threshold: usize) -> Self {
         self.threshold = threshold;
@@ -63,6 +97,9 @@ impl IdfIndexEntry {
     }
 }
 
+// SCANNS
+// TFIndexEntry stores the index associated with
+// a file, the frequency of a term in that file
 #[derive(Debug, Default)]
 pub struct TfIndexEntry {
     /// Index associated with file.
@@ -70,6 +107,10 @@ pub struct TfIndexEntry {
 
     /// Frequency of term in the file.
     frequency: usize,
+    // ---
+    // Not mentioned in paper.
+    // total file tokens param
+    // can be added here only
 }
 
 impl TfIndexEntry {
@@ -100,23 +141,23 @@ impl AppendCache {
 mod tests {
     use crate::{
         index::{IdfIndexEntry, TfIndexEntry},
-        util::Counter,
+        // util::Counter,
     };
 
     #[test]
     fn test_index_idf_entry_basic() {
         let idf = IdfIndexEntry::new(100);
         assert_eq!(idf.entries.capacity(), 100);
-        assert_eq!(*idf.file_count, 0);
+        // assert_eq!(*idf.file_count, 0);
         assert_eq!(idf.entries.len(), 0);
         assert_eq!(idf.threshold, 0);
     }
 
-    #[test]
-    fn test_index_idf_entry_with_file_count() {
-        let idf = IdfIndexEntry::new(100).with_file_count(Counter::new(5));
-        assert_eq!(*idf.file_count, 5);
-    }
+    // #[test]
+    // fn test_index_idf_entry_with_file_count() {
+    //     let idf = IdfIndexEntry::new(100).with_file_count(Counter::new(5));
+    //     assert_eq!(*idf.file_count, 5);
+    // }
 
     #[test]
     fn test_index_idf_entry_with_limit() {
